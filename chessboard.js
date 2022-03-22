2f06d090892f2a4452d07c5f19a9f4dc9837c5e9
ezepdev <edjpinheiro@gmail.com>
2022-03-21 23:32:28 -0300

    feature: add resolved chess exercise

---
+++ chessboard.js
@@ -0,0 +1,34 @@
+function isEven(nro) {
+  return nro % 2 === 0;
+}
+
+function buildRowEven(size) {
+  return buildRow(" ", "#", 8);
+}
+
+function buildRow(evenPosition, oddPosition, size) {
+  let row = "";
+  for (let nroCeld = 0; nroCeld < size; nroCeld++) {
+    row += isEven(nroCeld) ? evenPosition : oddPosition;
+  }
+  return row;
+}
+
+function buildRowOdd(size) {
+  return buildRow("#", " ", 8);
+}
+
+function insertNewline(string) {
+  return (string += "\n");
+}
+
+function buildChessboard(size) {
+  let chessboard = "\n";
+  for (let column = 0; column < size; column++) {
+    chessboard += isEven(column) ? buildRowEven(size) : buildRowOdd(size);
+    chessboard = insertNewline(chessboard);
+  }
+  return chessboard;
+}
+
+buildChessboard(8);
